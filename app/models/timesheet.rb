9223c2177b0f04ddc19c1709f00ecd76aad979cf
Suren91 <surendra.koripella242@gmail.com>
2014-08-28 23:26:52 -0400

    Updated to check for future date in timesheet

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -4,6 +4,7 @@ class Timesheet < ActiveRecord::Base
   belongs_to :project  
   validates :employee_id, :project_id, presence: true
   validates :duration, numericality: {greater_than: 0 }
+  validate :validate_dates
 
   def duration
     workend-workstart
@@ -11,6 +12,12 @@ class Timesheet < ActiveRecord::Base
   
   def duration_in_hrs
     duration.to_i/3600
+  end 
+  
+  def validate_dates
+     if self.workdate > Date.today
+	   errors.add(:workdate, "Work date cannot be in future")
+	 end 
   end 
 
 
